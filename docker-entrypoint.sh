1fdca908ae6179efb45f9519e001ef5c0bbaeee1
Gunter Zeilinger <gunterze@gmail.com>
2016-09-01 22:13:11 +0200

    dcm4che/dcm4chee-arc-light#318: Support alternatively using Standalone Keycloak server instead Wildfly Keycloak Overlay

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,10 @@ set -e
 if [ "$1" = 'standalone.sh' ]; then
     if [ ! -d $JBOSS_HOME/standalone/configuration ]; then
         cp -r /docker-entrypoint.d/configuration $JBOSS_HOME/standalone
+        sed -e "s%dc=dcm4che,dc=org%${LDAP_BASE_DN}%" \
+            -e "s%ldap://ldap:389%ldap://${LDAP_HOST}:${LDAP_PORT}%" \
+            -e "s%\"bindCredential\" : \"secret\"%\"bindCredential\" : \"${LDAP_ROOTPASS}\"%" \
+            -i $JBOSS_HOME/standalone/configuration/dcm4che-realm.json
         $JBOSS_HOME/bin/add-user.sh $WILDFLY_ADMIN_USER $WILDFLY_ADMIN_PASSWORD --silent
         $JBOSS_HOME/bin/add-user-keycloak.sh -r master -u $KEYCLOAK_ADMIN_USER -p $KEYCLOAK_ADMIN_PASSWORD
         chown -R keycloak:keycloak $JBOSS_HOME/standalone
