2b0b365f65fb2439cba89d28869fbfe364cbe940
Gunter Zeilinger <gunterze@gmail.com>
2019-05-28 08:45:25 +0200

    Docker images: provide ENVs to pass path of file with password dcm4che/dcm4chee-arc-light#1606

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,36 +4,7 @@ set -e
 
 if [ "$1" = 'standalone.sh' ]; then
 
-    # usage: file_env VAR [DEFAULT]
-    #    ie: file_env 'XYZ_DB_PASSWORD' 'example'
-    # (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
-    #  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
-    file_env() {
-        local var="$1"
-        local fileVar="${var}_FILE"
-        local def="${2:-}"
-        if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
-            echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
-            exit 1
-        fi
-        local val="$def"
-        if [ "${!var:-}" ]; then
-            val="${!var}"
-        elif [ "${!fileVar:-}" ]; then
-            val="$(< "${!fileVar}")"
-        fi
-        export "$var"="$val"
-        unset "$fileVar"
-    }
-
-    file_env 'LDAP_ROOTPASS' 'secret'
-    file_env 'KEYCLOAK_DB_USER' 'keycloak'
-    file_env 'KEYCLOAK_DB_PASSWORD' 'keycloak'
-    file_env 'KEYCLOAK_USER'
-    file_env 'KEYCLOAK_PASSWORD'
-    file_env 'KEYSTORE_PASSWORD' 'secret'
-    file_env 'KEY_PASSWORD' "${KEYSTORE_PASSWORD}"
-    file_env 'TRUSTSTORE_PASSWORD' 'secret'
+    . setenv.sh
 
     # Append '?' in the beginning of the string if KEYCLOAK_DB_JDBC_PARAMS value isn't empty
     KEYCLOAK_DB_JDBC_PARAMS=$(echo ${KEYCLOAK_DB_JDBC_PARAMS} | sed '/^$/! s/^/?/')
